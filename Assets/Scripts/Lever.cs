0bae09b968300983fbeee841ef221bf1cbed09ff
Wanchalerm Suksawat <wanchalerm.s@neosmart.co.th>
2021-10-05 22:42:13 +0700

    update mini map

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Lever : MonoBehaviour
 {
     private Animator anim;
+    public GameObject icon;
 
     private void Start()
     {
@@ -20,6 +21,7 @@ public class Lever : MonoBehaviour
 
             anim.SetTrigger("Unlock");
 
+            icon.SetActive(false);
             GetComponent<BoxCollider2D>().enabled = false;
             GetComponent<AudioSource>().Play();
         }
